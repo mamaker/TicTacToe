acc048e4548d111faca7949cf7017d602d5a5b47
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2019-04-03 18:35:41 -0500

    Added functionality to draw any square size board

--- TicTacToe.py
+++ TicTacToe.py
@@ -10,27 +10,45 @@ def drawBoard(board, labels = ("ABC", "123")):
 ##print(' 0 | 0 | 0 ') 
 ##print('---|---|---')
 ##print(' 0 | 0 | 0 ')
+#print(len(board[0]))
     colNames, rowNames = labels
-    print(' ')
-    print(' '*10 +'C O L U M N')
-    print(' '*11 +'{0}   {1}   {2}'.format(colNames[0],colNames[1],colNames[2]))
-    print(' ')
-    j = 0
-    for row in board:
-        x = ''
-        for i in row:
-            j += 1
-            if j % 3 == 1:
-                x +='Row {}'.format(rowNames[int(j/3)])+ ' '*5
-            x += ' {0} '.format(i)
-            if j % 3 != 0:
-                x += '|'
-            else:
-                print
-                print(x)
-                if j != 9:
-                    print((' '*10) +'---|---|---')
-    print(' ')
+    sideLen = len(colNames)
+    boardLen = len(board)
+    boardwidth = len(board[0])
+    lhs = (sideLen, sideLen, sideLen)
+    rhs = (len(rowNames), len(board), len(board[0]))
+    
+    if lhs == rhs:
+        print(' ')
+        print(' '*10 +'C O L U M N')
+        colHead = ''
+        for col in colNames:
+            colHead += col+(' '*3)
+        print(' '*11 +colHead)
+            
+        print(' ')
+        j = 0
+        for row in board:
+            x = ''
+            for i in row:
+                j += 1
+                if j % sideLen == 1:
+                    x +='Row {}'.format(rowNames[int(j/sideLen)])+ ' '*5
+                x += ' {0} '.format(i)
+                if j % sideLen != 0:
+                    x += '|'
+                else:
+                    print(x)
+                    if j != sideLen**2:
+                        divider = (' '*10)
+                        for k in range(sideLen):
+                            divider += '---'
+                            if k < sideLen -1:
+                                divider += '|'
+                        print(divider)
+        print(' ')
+    else:
+        print('Sorry, can only draw square gameboards.')
     return
 
 def checkWinner(board, plyr):
--- drawBoard.py
+++ drawBoard.py
@@ -1,8 +1,8 @@
 
 
-def drawBoard(board):
+def drawBoard(board, labels = ("ABC", "123")):
     """ Draws Game board """
-##for row in game:
+##for row in board:
 ##    print(row)
 ##
 ##print(' 0 | 0 | 0 ') 
@@ -10,34 +10,50 @@ def drawBoard(board):
 ##print(' 0 | 0 | 0 ') 
 ##print('---|---|---')
 ##print(' 0 | 0 | 0 ')
-    print(' ')
-    print(' '*10 +'C O L U M N')
-    print(' '*11 +'A   B   C')
-    print(' ')
-    j = 0
-    for row in board:
-        x = ''
-        for i in row:
-            j += 1
-            if j % 3 == 1:
-                x +='Row {}'.format(int(j/3)+1)+ ' '*5
-            x += ' {0} '.format(i)
-            if j % 3 != 0:
-                x += '|'
-            else:
-                print
-                print(x)
-                if j != 9:
-                    print((' '*10) +'---|---|---')
-    print(' ')
+#print(len(board[0]))
+    colNames, rowNames = labels
+    sideLen = len(colNames)
+    boardLen = len(board)
+    boardwidth = len(board[0])
+    lhs = (sideLen, sideLen, sideLen)
+    rhs = (len(rowNames), len(board), len(board[0]))
+    
+    if lhs == rhs:
+        print(' ')
+        print(' '*10 +'C O L U M N')
+        colHead = ''
+        for col in colNames:
+            colHead += col+(' '*3)
+        print(' '*11 +colHead)
+            
+        print(' ')
+        j = 0
+        for row in board:
+            x = ''
+            for i in row:
+                j += 1
+                if j % sideLen == 1:
+                    x +='Row {}'.format(rowNames[int(j/sideLen)])+ ' '*5
+                x += ' {0} '.format(i)
+                if j % sideLen != 0:
+                    x += '|'
+                else:
+                    print(x)
+                    if j != sideLen**2:
+                        divider = (' '*10)
+                        for k in range(sideLen):
+                            divider += '---'
+                            if k < sideLen -1:
+                                divider += '|'
+                        print(divider)
+        print(' ')
+    else:
+        print('Sorry, can only draw square gameboards.')
     return
 
 
-game = [[0,0,0],
-        [0,0,0],
-        [0,0,0]]
-board = [["0","0","0"],
-        ["0","0","0"],
-        ["0","0","0"]]
-
-drawBoard(board)
+board = [["0","0","0","0"],
+         ["0","0","0","0"],
+         ["0","0","0","0"],
+         ["0","0","0","0"]]
+drawBoard(board, labels = ("ABCD", "1234"))
