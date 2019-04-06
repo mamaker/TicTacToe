cb168b8d6d7043d6e62856eac550ca3f4454e788
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2019-04-06 11:13:24 -0500

    Enabled players to choose unique initials to play with.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # TicTacToe
-This is some Python Game Code.
-Without using the Graphics Packages.
+This popular Game is written in Python 3.x,
+without using the Graphics Packages.
+It allows dynamic, interactive choice of Gameboard size.
+It also allows players to choose unique initials to markup their moves on the gameboard.
+It also allows continuing with a fresh new game after a game is won.
+Enjoy!
+
 
--- TicTacToe.py
+++ TicTacToe.py
@@ -176,7 +176,23 @@ def blankBoard(board):
     return True
 
 
-def getChoice(prompt, options):
+def getChoice(prompt, options, showOptions = ''):
+    """ Get input from User """
+
+    choice = ''
+    if showOptions == '':
+        showOptions = options
+    try:
+        while (len(choice) != 1) or (choice.upper() not in tuple(showOptions+'Q')):
+            choice = input('{0} {1} or Q to quit: '.format(prompt,tuple(showOptions)))
+    except:
+        print('OOOPS! error')
+        choice = 'Q'
+        
+    return choice.upper()
+
+
+def getChoice2(prompt, options):
     """ Get input from User """
 
     choice = ''
@@ -190,7 +206,59 @@ def getChoice(prompt, options):
     return choice.upper()
 
 
+def getPlyrs():
+    """Allow players to choose unique initials
+        for markup of individuals's position in Gameboard"""
+
+    letrs1 = 'ABCDEFGHIJKLMNOPRSTUVWXYZ'
+    showOpts = 'A>Z'
+    prompt = 'What unique alphabet Initial, Player#'
+    initl1 = ''
+    while initl1 == '' or initl1 not in letrs1+'Q': 
+        initl1 = getChoice(prompt+' 1?',letrs1, showOpts)
+
+    if initl1 == 'A':
+        showOpts = 'B>Z'
+    elif initl1 == 'Z':
+        showOpts = 'A>Y'
+        
+    x = list(letrs1+'Q')
+    del x[x.index(initl1)]
+    letrs2 = ''
+    for letr in x:
+        letrs2 += letr    
+    
+    initl2 = initl1
+    while initl1 != 'Q' and initl2 != 'Q' and  (initl1 == initl2 or initl2 not in letrs2):
+        initl2 = getChoice(prompt+' 2?',letrs2, showOpts)
+        
+    return initl1,initl2
+
+
+def getPlyrs2():
+    """Allow players to choose initials
+        for markup of individuals's position in Gameboard"""
+    
+    letrs1 = 'ABCDEFGHIJKLMNOPRSTUVWXYZ'
+    prompt = 'What unique alphabet Initial, Player#'
+    initl1 = getChoice(prompt+' 1?',letrs1)
+    
+    x = list(letrs1+'Q')
+    del x[x.index(initl1)]
+    letrs2 = ''
+    for letr in x:
+        letrs2 += letr    
+
+    initl2 = initl1
+    while initl1 != 'Q' and initl2 != 'Q' and  initl1 == initl2:
+        initl2 = getChoice(prompt+' 2?',letrs2)
+        
+    return initl1,initl2
+
 def getSize():
+    """ Allow user to choose the Gameboard size
+    from 3 to 9 square"""
+    
     size = 0
     sizes = '3456789'
     siz = getChoice('What size Game board?', sizes)
@@ -206,29 +274,32 @@ def main():
         Gameboard size Configurable dynamically."""
     #sideLen = 4
     sideLen = getSize()
-    if sideLen > 2 and sideLen < 10:
-        colNames = 'ABCDEFGHI'[:sideLen]
-        rowNames = '123456789'[:sideLen]
-        labels = (colNames, rowNames)
-        blnk = ' '
-        board = [[blnk for i in range(sideLen)] for j in range(sideLen)]
-        nxtPlyr = {"X":"O","O":"X"}
-        plyr = "X"
-        validMove = False
-        newGame = True
-        nextMove = ''
-        while nextMove != 'Q': 
-            drawBoard(board, labels)
-            nextMove = getMove(plyr, validMove, newGame, labels)
-            newGame = False
-            if nextMove != 'Q':
-                validMove = makeMove(plyr, nextMove, board, labels)
-                if validMove: 
-                    if checkWinner(board, plyr):
-                        declareWinner(plyr, board, labels)
-                        newGame = blankBoard(board)
-                        nextMove = getChoice('One more Game?','Yy')
-                    plyr = nxtPlyr[plyr]
+    if sideLen > 2 and sideLen < 10: 
+        plr1, plr2 = getPlyrs()
+    
+        if plr1 != 'Q' and plr2 != 'Q':
+            colNames = 'ABCDEFGHI'[:sideLen]
+            rowNames = '123456789'[:sideLen]
+            labels = (colNames, rowNames)
+            blnk = ' '
+            board = [[blnk for i in range(sideLen)] for j in range(sideLen)]
+            switchPlyr = {plr1:plr2,plr2:plr1}
+            plyr = plr1
+            validMove = False
+            newGame = True
+            nextMove = ''
+            while nextMove != 'Q': 
+                drawBoard(board, labels)
+                nextMove = getMove(plyr, validMove, newGame, labels)
+                newGame = False
+                if nextMove != 'Q':
+                    validMove = makeMove(plyr, nextMove, board, labels)
+                    if validMove: 
+                        if checkWinner(board, plyr):
+                            declareWinner(plyr, board, labels)
+                            newGame = blankBoard(board)
+                            nextMove = getChoice('One more Game?','Yy')
+                        plyr = switchPlyr[plyr]
     sayBye()
    
          
